165dc4ca17245e32996f323095fcc1f2eb1e9442
GabrielaMGC <Gmgc2010@gmail.com>
2022-08-11 14:59:00 -0300

    Subindo o cinema

--- App.js
+++ App.js
@@ -1,11 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+  const [nome,setNome] = useState('-Melância-');
+  const [sessao, setSessao] = useState('Stranger Things');
+  const [cor, setCor] = useState('#150526')
+  const [exibindo,setExipindo] = useState(true)
+  const clicado = () =>{
+      setNome("-Sorvete-");
+      setSessao("Gilmore Girls");
+      setExipindo(false);
+  } 
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View styles={{backgroundColor: `{cor}`}}>
+        <Text style={styles.titulo}>{nome}</Text>
+        <Text style={styles.paragrafo}>{sessao}</Text>
+        <Text style={styles.paragrafo}>{exibindo ? "Está em sessão" : "Acabou"}</Text>
+        <StatusBar style="auto" />
+        <Button title ='trocar sessão' onPress={clicado} />
+      </View>
     </View>
   );
 }
@@ -13,8 +29,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#150526',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  titulo:{
+    color: '#F25C05',
+    fontSize: 28,
+  },
+  paragrafo:{
+    fontSize:12,
+    color:'#F20505',
+  }
 });
