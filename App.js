d8098e051ee862dc5ec40395248d285c03972c2e
GabrielaMGC <gmgc2010@gmail.com>
2022-08-25 14:43:31 -0300

    finalizando o pokemon

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { SafeAreaView, FlatList, Image, StyleSheet, Text, View, Button } from 'react-native';
 import { useState, useEffect } from 'react';
+import styled from 'styled-components/native';
 
 export default function App() {
   const [pokemons, setPokemons] = useState([])
@@ -34,28 +35,53 @@ function PokemonShow(item){
   const ImageUrl = 'https://cdn.traction.one/pokedex/pokemon' +pokemonNumber+'.png'
   
   return(
-    <View style={{flexDirection:'row'}}>
-      <Image style={{width:100, height:100}}
+    <View style={pokemonEstilo.card}>
+      <NomePokemon>{name}</NomePokemon>
+      <Image style={pokemonEstilo.imagem}
              source={{uri: ImageUrl.replace('/.png','.png')}}
       />
-      <Text>{name}</Text>
     </View>
   )
 }
 
+const pokemonEstilo = StyleSheet.create({
+  card: {
+    flexDirection: 'column',
+    width: '350',
+    height: '350',
+    backgroundColor: '#730202',
+    margin: 25,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 15,
+  },
+
+  imagem:{
+    width: 300,
+    height: 300,
+
+  }
+})
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#150526',
+    backgroundColor: '#A2A638',
     alignItems: 'center',
     justifyContent: 'center',
   },
   titulo:{
-    color: '#F25C05',
+    color: '#334018',
     fontSize: 28,
   },
   paragrafo:{
     fontSize:12,
-    color:'#F20505',
+    color:'#D9C55F',
   }
 });
+
+const NomePokemon = styled.Text`
+  color: #44732F;
+  font-size: 30px;
+
+`;
