7fef16166f3e929c245d5cdcc29df010a3ed5afb
GabrielaMGC <gmgc2010@gmail.com>
2022-09-08 15:22:46 -0300

    pegando os dados

--- App.js
+++ App.js
@@ -2,14 +2,15 @@ import { SafeAreaView, FlatList, Image, StyleSheet, Text, View, Button } from 'r
 import { useState, useEffect } from 'react';
 import styled from 'styled-components/native';
 import ListItem from './components/ListItem';
+import { SAMPLE_DATA } from './assets/data/sampleData';
 
 export default function App() {
 
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.titleWrapper}>
+     
         <Text style={styles.text}> Markers </Text>
-      </View>
+     
       <View style={styles.divider}>
 
       </View>
--- components/ListItem.js
+++ components/ListItem.js
@@ -1,43 +1,58 @@
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
+import { View, Text,TouchableOpacity,StyleSheet,Image } from 'react-native'
 import React from 'react'
+import icon from '../Imagens/img1.png'
 
-const ListItem = () => {
-  return (
+const ListItem = ({name, symbol, currentPrice, priceChangePercentage7d, logoUrl}) => {
+    const priceChangeColor = priceChangePercentage7d > 0 ? 'green' : 'red'
+
+    return (
     <TouchableOpacity>
-        <View style={StyleSheet.itemWrapper}>
-            <View style={style.leftWrapper}>
-                <Image source={{uri: logoUrl}} style={styles.image}/>
-                    <View style={styles.titleWrapper}>
-                        <Text style={styles.title}> Etherium </Text>
-                        <Text style={styles.subtitle}> ETH </Text>
-                    </View>
-                    </View>
-                    <View style={styles.rightWrapper}>
-                        <Text style={styles.title}> Etherium </Text>
-                        <Text style={[styles.subtitle,{color: 'red'}]}> ETH </Text>
-                    </View>
+      <View style={styles.itemWrapper}>
+        <View style={styles.leftWrapper}>
+          <Image source= {{uri: logoUrl}} style={styles.image}/>
+          <View style= {styles.titlesWrapper}>
+            <Text style={styles.title}>{name}</Text>
+            <Text style={styles.subtitle}>{symbol.toUpperCase}</Text>
+          </View>
         </View>
+        <View style={styles.rightWrapper}>
+            <Text style={styles.title}>{currentPrice}</Text>
+            <Text style={[styles.subtitle,{color: priceChangeColor}]}>{priceChangePercentage7d}</Text>
+        </View>
+      </View>
     </TouchableOpacity>
   )
 }
 const styles = StyleSheet.create({
-    itemWrapper:{
-        paddingHorizontal:16,
-        marginTop: 24,
-        flexDirection: "row",
-        justifyContent: "space-between",
-        alignItems: 'center',
-    },
-    leftWrapper:{
-        flexDirection: 'row',
-        alingItems: 'center',
-    },
-    rightWrapper:{
-        alignItems: 'flex-end',
-    },
-    image:{},
-    titlesWrapper:{},
-    title:{},
-    subtitle:{},
+  itemWrapper:{
+    paddingHorizontal:16,
+    marginTop: 24,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems:  'center',
+  },
+  leftWrapper:{
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  rightWrapper:{
+    alignItems: 'flex-end',
+  },
+  image: {
+    width:48,
+    height:48,
+  },
+  titlesWrapper:{
+    marginLeft:8
+  },
+  title: {
+    fontSize:18,
+    color: '#C5D0D9',
+  },
+  subtitle: {
+    marginTop:4,
+    fontSize:14,
+    color: '#737373',
+  },
 })
 export default ListItem
